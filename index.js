32cc3d5dabe3ff1558d8111e988162cd6f1f35e7
Pedro Vicente Gómez Sánchez <pedrovicente.gomez@gmail.com>
2018-10-22 16:14:09 +0200

    Get the app working with just react native and without expo

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import App from './build/App';
 
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 
-AppRegistry.registerComponent('katasuperheroesreactnative', () => App);
+AppRegistry.registerComponent('KataSuperHeroesReactNative', () => App);
--- ios/KataSuperHeroesReactNativeTests/KataSuperHeroesReactNativeTests.m
+++ ios/KataSuperHeroesReactNativeTests/KataSuperHeroesReactNativeTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface katasuperheroesreactnativeTests : XCTestCase
+@interface KataSuperHeroesReactNativeTests : XCTestCase
 
 @end
 
-@implementation katasuperheroesreactnativeTests
+@implementation KataSuperHeroesReactNativeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
