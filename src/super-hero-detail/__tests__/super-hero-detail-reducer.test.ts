2b30a6383f1747c5254690101d92586da673e51f
Pedro Vicente Gómez Sánchez <pedrovicente.gomez@gmail.com>
2019-01-10 15:39:29 +0100

    Add another tests that's not working at all

--- src/super-hero-detail/__tests__/super-hero-detail-reducer.test.ts
+++ src/super-hero-detail/__tests__/super-hero-detail-reducer.test.ts
@@ -1,7 +1,17 @@
 import { Store } from "redux";
 import appStore from "../../store";
-import { anySuperHeroId } from "../../test-doubles/super-heroes";
+import { anyRegularSuperHero, anySuperHeroId } from "../../test-doubles/super-heroes";
 import { fetchSuperHeroById } from "../super-hero-detail-actions";
+
+jest.mock("../../core/super-heroes-repository", () => {
+    return {
+        getSuperHeroById: jest.fn(),
+    };
+});
+
+import { Option, Some } from "fp-ts/lib/Option";
+import { SuperHero } from "../../core/model";
+import { getSuperHeroById } from "../../core/super-heroes-repository";
 
 it("creates the store using the initial state", () => {
     const store = appStore();
@@ -18,10 +28,26 @@ it("mark the loading property as true while fetching the super hero by id", () =
     snapStoreState(store);
 });
 
+it("mark the loading property as false and store the super hero fetched", () => {
+    const store = appStore();
+    givenThereIsASuperHero();
+
+    store.dispatch(fetchSuperHeroById(anySuperHeroId));
+
+    snapStoreState(store);
+});
+
 function snapStoreState(store: Store) {
     expect(store.getState().superHeroDetail).toMatchSnapshot();
 }
 
 function givenTheRepositoryTakesTimeToAnswer() {
 
+}
+
+function givenThereIsASuperHero() {
+    const mock = getSuperHeroById as jest.Mock<Promise<Option<SuperHero>>>;
+    
+    mock.mockReturnValueOnce(Promise.resolve(new Some(anyRegularSuperHero)));
+    console.log("-------> " + JSON.stringify(mock("id")));
 }
